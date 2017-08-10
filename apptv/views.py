95023ee66af370c2d2c242fb411b3dfcedec8726
923401910@qq.com <923401910@qq.com>
2017-08-10 14:14:23 +0800

    提交项目

---
+++ apptv/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+import datetime
+
+
+# Create your views here.
+
+class IndexView(View):
+    def get(self, request, tvno='0'):
+        tv_list = [
+            {'name': '中视新闻', 'tvcode': '3e1q8LorZ2U'},
+            {'name': '民视新闻', 'tvcode': 'XxJKnDLYZz4'},
+            {'name': '三立新闻', 'tvcode': '15IKxpj1gQA'},
+        ]
+
+        now = datetime.datetime.now()
+        tvno = tvno
+        tv = tv_list[int(tvno)]
+        return render(request, 'index.html', locals())
--- django_youtube/urls.py
+++ django_youtube/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from apptv import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',views.IndexView.as_view(),name='index'),
+    url(r'^(\d{1})/$',views.IndexView.as_view(),name='tv-url'),
 ]
